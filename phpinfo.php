b4cc7d6f74b3427769e4757d030ef3cd3e7329fa
visonforcoding <caowenpeng1990@126.com>
2016-09-20 11:34:42 +0800

    first commit

---
+++ phpinfo.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+* Encoding     :   UTF-8
+* Created on   :   2016-9-18 15:25:30 by caowenpeng , caowenpeng1990@126.com
+*/
+
+phpinfo();
---
+++ websocket.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Encoding     :   UTF-8
+ * Created on   :   2016-9-18 12:19:55 by caowenpeng , caowenpeng1990@126.com
+ */
+$serv = new swoole_websocket_server("0.0.0.0", 9502);
+//$serv->set(array('daemonize' => true));
+$serv->on('Open', function($server, $req) {
+    echo "connection open: " . $req->fd;
+    echo "connection counts: " . count($server->connections)."\r\n";
+});
+
+$serv->on('Message', function($server, $frame) {
+    echo "message: " . $frame->data . "\r\n";
+//    $server->push($frame->fd, json_encode(["hello", "world"]));
+    var_dump($server->connections);
+    foreach ($server->connections as $fd) {
+        $server->push($fd, json_encode($frame->data,JSON_UNESCAPED_UNICODE));
+    }
+});
+
+$serv->on('Close', function($server, $fd) {
+    echo "connection close: " . $fd."\r\n";
+    echo "connection counts: " . count($server->connections)."\r\n";
+});
+
+$serv->start();
