be854b6da0868d3fd5561ed2c06ff3130abca65e
visonforcoding <caowenpeng1990@126.com>
2016-09-26 16:13:13 +0800

    完成房间信息、在线用户列表，用户退出业务待完成，代码待重构一部分

--- data.php
+++ data.php
@@ -6,15 +6,17 @@ $avatar_path = dirname(__FILE__) . '/resource/avatar';
 //$avatars = directory_tree($avatar_path);
 $userinfo = [];
 
-$redis_host = '192.168.33.10';
+$redis_host = '192.168.1.7';
 $redis_port = '6379';
 $redis = new \Redis();
 $redis->connect($redis_host,$redis_port);
-$avatars = file_get_contents('userinfo.json');
-var_dump(json_decode($avatars));
-foreach(json_decode($avatars) as $avatar){
-    $redis->sAdd('chat_userinfo',  json_encode($info));
-}
+//$avatars = file_get_contents('userinfo.json');
+//var_dump(json_decode($avatars));
+//foreach(json_decode($avatars) as $avatar){
+//    $redis->sAdd('chat_userinfo',  json_encode($info));
+//}
+  $userlist = $redis->hGetAll('chat_room_user');
+  var_dump($userlist);
 //foreach ($avatars as $avatar) {
 //    if (preg_match('/120px\-(.*)\./', $avatar['name'], $matches)) {
 //        $name = $matches[1];
@@ -30,4 +32,3 @@ foreach(json_decode($avatars) as $avatar){
 //    $redis->sAdd('chat_userinfo',  json_encode($info));
 //}
 //file_put_contents('userinfo.json',  json_encode($userinfo));
-
--- websocket.php
+++ websocket.php
@@ -8,14 +8,15 @@ $serv = new swoole_websocket_server("0.0.0.0", 9502);
 //$serv->set(array('daemonize' => true));   //以守护进程运行'
 //配置区
 
-$redis_host = '192.168.33.10';
+$redis_host = '192.168.1.7';
 $redis_port = '6379';
 $redis = new \Redis();
 $redis->connect($redis_host, $redis_port);
 
 $room_admin = [
     'nick' => '曹麦穗',
-    'avatar' => 'avatar_1.jpg'
+    'avatar' => 'avatar_1.jpg',
+    'create_time'=>date('Y-m-d H:i')
 ];
 //每次启动 server 初始化数据  还原用户库 清空聊天室用户
 $avatars = file_get_contents('userinfo.json');
@@ -25,17 +26,46 @@ foreach(json_decode($avatars) as $avatar){
 $redis->del('chat_room_user');
 $serv->on('Open', function($server, $req)use($redis,$room_admin) {
     //$req 对象是 server_http_request  $req->fd 属性 为客户端请求id 此id可用作push的发送对象
-    $user = $redis->sPop('chat_userinfo');  //从昵称库中随机找一个用户 并删除
-    $userinfo = json_decode($user);
-    $redis->hSet('chat_room_user', $req->fd, $user);  //加入到聊天室
+    $newuser = $redis->sPop('chat_userinfo');  //从昵称库中随机找一个用户 并删除
+    $userinfo = json_decode($newuser);
+    $userlist = $redis->hGetAll('chat_room_user');
+    //初始化 在线用户列表
+    $userListData = [
+        'msgtype'=>'userlist',
+        'create_time'=>$room_admin['create_time'],
+        'profile'=>[
+            'nick'=>$userinfo->nick,
+            'avatar'=>$userinfo->avatar
+        ]
+    ];
+    foreach ($userlist as $fd=>$user){
+        $item = json_decode($user);
+        $userListData['items'][] = [
+          'fd'=>$fd,
+          'nick'=>$item->nick,
+          'avatar'=>$item->avatar,
+        ];
+    }
+    $server->push($req->fd,json_encode($userListData, JSON_UNESCAPED_UNICODE)); //向新加入的用户 发送在线列表
+    $redis->hSet('chat_room_user', $req->fd, $newuser);  //加入到聊天室
     $data = [
+        'msgtype'=>'msg',    //消息类型
         'body' => '欢迎'.$userinfo->nick.'加入群聊!',
         'nick' => $room_admin['nick'],
         'avatar' => $room_admin['avatar'],
         'timestamp' => time(),
         'unique' => md5(uniqid()),
     ];
+    $newUserData = [
+        'msgtype'=>'newuser',    //消息类型
+        'body' => '新用户加入',
+        'nick' => $userinfo->nick,
+        'avatar' => $userinfo->avatar,
+        'timestamp' => time(),
+        'unique' => md5(uniqid()),
+    ];
     foreach ($server->connections as $fd) {
+        $server->push($fd, json_encode($newUserData, JSON_UNESCAPED_UNICODE));
         $server->push($fd, json_encode($data, JSON_UNESCAPED_UNICODE));
     }
     //echo "connection open: " . $req->fd;
@@ -49,6 +79,7 @@ $serv->on('Message', function($server, $frame)use($redis) {
         $user = $redis->hGet('chat_room_user',$frame->fd);
         $userinfo = json_decode($user);
         $data = [
+            'msgtype'=>'msg',
             'body' => $frame->data,
             'nick' => $userinfo->nick,
             'avatar' => $userinfo->avatar,
@@ -59,9 +90,37 @@ $serv->on('Message', function($server, $frame)use($redis) {
     }
 });
 
-$serv->on('Close', function($server, $fd) {
+$serv->on('Close', function($server, $fd)use($redis,$room_admin) {
     echo "connection close: " . $fd . "\r\n";
     echo "connection counts: " . count($server->connections) . "\r\n";
+    $user = $redis->hGet('chat_room_user',$fd);
+    $userinfo = json_decode($user);
+    $redis->hDel('chat_room_user',$fd);
+    $redis->sAdd('chat_userinfo',$userinfo);
+    $data = [
+        'msgtype'=>'msg',    //消息类型
+        'body' => $userinfo->nick.'退出了群聊!',
+        'nick' => $room_admin['nick'],
+        'avatar' => $room_admin['avatar'],
+        'timestamp' => time(),
+        'unique' => md5(uniqid()),
+    ];
+    $outUserData = [
+        'msgtype'=>'userout',    //消息类型
+        'body' => '新用户加入',
+        'nick' => $userinfo->nick,
+        'avatar' => $userinfo->avatar,
+        'timestamp' => time(),
+        'unique' => md5(uniqid()),
+    ];
+    foreach ($server->connections as $f) {
+        if($f==$fd){
+            continue;
+        }
+        $server->push($f, json_encode($outUserData, JSON_UNESCAPED_UNICODE));
+        $server->push($f, json_encode($data, JSON_UNESCAPED_UNICODE));
+    }
+    
 });
 
 $serv->start();
